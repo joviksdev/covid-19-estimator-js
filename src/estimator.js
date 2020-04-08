059c1ec30653236014fe64b48b16b98e7136e746
joviksdev <toch.emeo@gmail.com>
2020-04-08 20:31:51 +0100

    estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,41 @@
-const covid19ImpactEstimator = (data) => data;
+import computeNumberOfDays from './computation';
+
+const covid19ImpactEstimator = inputData => {
+  const {
+    region,
+    periodType,
+    timeToElapse,
+    reportedCases,
+    population,
+    totalHospitalBeds
+  } = inputData;
+
+  const data = {};
+  const impact = {};
+  const severeImpact = {};
+
+  // Create properties to impcat and severeImpact and assign value to them
+
+  impact.currentlyInfected = reportedCases * 10;
+  severeImpact.currentlyInfected = reportedCases * 50;
+
+  impact.infectionsByRequestedTime = computeNumberOfDays(
+    currentlyInfected,
+    timeToElapse
+  );
+
+  severeImpact.infectionsByRequestedTime = computeNumberOfDays(
+    currentlyInfected,
+    timeToElapse
+  );
+
+  const output = {
+    data,
+    impact,
+    severeImpact
+  };
+
+  return output;
+};
 
 export default covid19ImpactEstimator;
