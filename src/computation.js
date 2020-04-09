fbc185d0426a57d4f4542780adcf34c5dfaef348
joviksdev <toch.emeo@gmail.com>
2020-04-09 08:56:28 +0100

    Break computation.js into resources

--- src/computation.js
+++ src/computation.js
@@ -1,10 +1,29 @@
-const computeNumberOfDays = (value, days) => {
+// Normalise the duration in day
+
+const normaliseDuration = (time, periodType) => {
+  switch (periodType) {
+    case 'months':
+      return time * 30;
+
+    case 'weeks':
+      return time * 7;
+
+    case 'days':
+      return time;
+
+    default:
+      return null;
+  }
+};
+
+const infectedEstimation = (infected, time) => {
   // currentlyInfected as value is doubles every 3 days
 
   // Number of set in a day (fators)
-  const factor = Math.round(days / 3);
-
-  return value * 2 ** factor;
+  const factor = Math.round(time / 3);
+  return infected * 2 ** factor;
 };
 
-export default computeNumberOfDays;
+const percentEstimate = (percent, value) => percent * value;
+
+export { normaliseDuration, infectedEstimation, percentInfectedEstimate };
