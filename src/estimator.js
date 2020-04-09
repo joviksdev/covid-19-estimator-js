70418f793469fe8aa5572e905505979a0deb6165
joviksdev <toch.emeo@gmail.com>
2020-04-09 09:57:34 +0100

    estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -1,6 +1,7 @@
 import { normaliseDuration, infectedEstimation, percentEstimate } from './computation';
 
 const covid19ImpactEstimator = (data) => {
+
   const {region, periodType, timeToElapse, reportedCases, population, totalHospitalBeds } = data;
 
   const impact = {};
@@ -12,43 +13,25 @@ const covid19ImpactEstimator = (data) => {
   impact.currentlyInfected = reportedCases * 10;
   severeImpact.currentlyInfected = reportedCases * 50;
 
-  impact.infectionsByRequestedTime = infectedEstimation(
-    impact.currentlyInfected,
-    normaliseTimeElapse
-  );
+  // Set properties of Impact and SevereImpact
+  impact.infectionsByRequestedTime = infectedEstimation(impact.currentlyInfected, normaliseTimeElapse);
+  severeImpact.infectionsByRequestedTime = infectedEstimation(severeImpact.currentlyInfected, normaliseTimeElapse);
 
-  severeImpact.infectionsByRequestedTime = infectedEstimation(
-    severeImpact.currentlyInfected,
-    normaliseTimeElapse
-  );
-
-  impact.severeCasesByRequestedTime = percentEstimate(
-    0.15,
-    impact.infectionsByRequestedTime
-  );
-
-  severeImpact.severeCasesByRequestedTime = percentEstimate(
-    0.15,
-    impact.infectionsByRequestedTime
-  );
+  impact.severeCasesByRequestedTime = percentEstimate(0.15, impact.infectionsByRequestedTime);
+  severeImpact.severeCasesByRequestedTime = percentEstimate(0.15, impact.infectionsByRequestedTime);
 
   impact.hospitalBedsByRequestedTime = percentEstimate(0.35, totalHospitalBeds) - impact.severeCasesByRequestedTime;
-
   severeImpact.hospitalBedsByRequestedTime = percentEstimate(0.35, totalHospitalBeds) - impact.severeCasesByRequestedTime;
 
   impact.casesForICUByRequestedTime = percentEstimate(0.05, impact.infectionsByRequestedTime);
-
   severeImpact.casesForICUByRequestedTime = percentEstimate(0.05, impact.infectionsByRequestedTime);
-
   impact.casesForVentilatorsByRequestedTime = percentEstimate(0.02, impact.infectionsByRequestedTime);
-
   severeImpact.casesForVentilatorsByRequestedTime = percentEstimate(0.02, impact.infectionsByRequestedTime);
 
   const dollarsInFlight = {};
   const { avgDailyIncomeInUSD, avgDailyIncomePopulation } = region;
 
   dollarsInFlight.impact = impact.infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * normaliseTimeElapse;
-
   dollarsInFlight.severeImpact = severeImpact.infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * normaliseTimeElapse;
 
 
