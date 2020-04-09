c20f0772619280dbad5b091d277dae936a34ce2c
Emmanuel J. Tochukwu <53878522+joviksdev@users.noreply.github.com>
2020-04-09 09:29:53 +0100

    Update estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -1,8 +1,4 @@
-import {
-  normaliseDuration,
-  infectedEstimation,
-  percentEstimate
-} from './computation';
+import { normaliseDuration, infectedEstimation, percentEstimate } from './computation';
 
 const covid19ImpactEstimator = (data) => {
   const {
@@ -44,33 +40,17 @@ const covid19ImpactEstimator = (data) => {
     impact.infectionsByRequestedTime
   );
 
-  impact.hospitalBedsByRequestedTime =
-    percentEstimate(0.35, totalHospitalBeds) -
-    impact.severeCasesByRequestedTime;
+  impact.hospitalBedsByRequestedTime = percentEstimate(0.35, totalHospitalBeds) - impact.severeCasesByRequestedTime;
 
-  severeImpact.hospitalBedsByRequestedTime =
-    percentEstimate(0.35, totalHospitalBeds) -
-    impact.severeCasesByRequestedTime;
+  severeImpact.hospitalBedsByRequestedTime = percentEstimate(0.35, totalHospitalBeds) - impact.severeCasesByRequestedTime;
 
-  impact.casesForICUByRequestedTime = percentEstimate(
-    0.05,
-    impact.infectionsByRequestedTime
-  );
+  impact.casesForICUByRequestedTime = percentEstimate(0.05, impact.infectionsByRequestedTime);
 
-  severeImpact.casesForICUByRequestedTime = percentEstimate(
-    0.05,
-    impact.infectionsByRequestedTime
-  );
+  severeImpact.casesForICUByRequestedTime = percentEstimate(0.05, impact.infectionsByRequestedTime);
 
-  impact.casesForVentilatorsByRequestedTime = percentEstimate(
-    0.02,
-    impact.infectionsByRequestedTime
-  );
+  impact.casesForVentilatorsByRequestedTime = percentEstimate(0.02, impact.infectionsByRequestedTime);
 
-  severeImpact.casesForVentilatorsByRequestedTime = percentEstimate(
-    0.02,
-    impact.infectionsByRequestedTime
-  );
+  severeImpact.casesForVentilatorsByRequestedTime = percentEstimate(0.02, impact.infectionsByRequestedTime);
 
   const dollarsInFlight = {};
   const { avgDailyIncomeInUSD, avgDailyIncomePopulation } = region;
@@ -79,13 +59,8 @@ const covid19ImpactEstimator = (data) => {
 
   dollarsInFlight.severeImpact = severeImpact.infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * normaliseTimeElapse;
 
-  const output = {
-    data,
-    impact,
-    severeImpact
-  };
 
-  return { ...output, dollarsInFlight };
+  return { data, impact, severeImpact, dollarsInFlight };
 };
 
 export default covid19ImpactEstimator;
