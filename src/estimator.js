0b14ced51fbd3ffd032c786e0fbb57d70cf2f6c4
joviksdev <toch.emeo@gmail.com>
2020-04-09 11:03:55 +0100

    estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -1,41 +1,82 @@
-import { normaliseDuration, infectedEstimation, percentEstimate } from './computation';
+import {
+  normaliseDuration,
+  infectedEstimation,
+  percentEstimate
+} from './computation';
 
 const covid19ImpactEstimator = (data) => {
-
-  const {region, periodType, timeToElapse, reportedCases, population, totalHospitalBeds } = data;
+  const {
+    region,
+    periodType,
+    timeToElapse,
+    reportedCases,
+    population,
+    totalHospitalBeds
+  } = data;
 
   const impact = {};
   const severeImpact = {};
 
   // Initialize normalise duration
-  const normaliseTimeElapse = normaliseDuration(timeToElapse, periodType);
+  const normaliseTime = normaliseDuration(timeToElapse, periodType);
 
   impact.currentlyInfected = reportedCases * 10;
   severeImpact.currentlyInfected = reportedCases * 50;
 
   // Set properties of Impact and SevereImpact
-  impact.infectionsByRequestedTime = infectedEstimation(impact.currentlyInfected, normaliseTimeElapse);
-  severeImpact.infectionsByRequestedTime = infectedEstimation(severeImpact.currentlyInfected, normaliseTimeElapse);
+  impact.infectionsByRequestedTime = infectedEstimation(
+    impact.currentlyInfected,
+    normaliseTime
+  );
+  severeImpact.infectionsByRequestedTime = infectedEstimation(
+    severeImpact.currentlyInfected,
+    normaliseTime
+  );
 
-  impact.severeCasesByRequestedTime = percentEstimate(0.15, impact.infectionsByRequestedTime);
-  severeImpact.severeCasesByRequestedTime = percentEstimate(0.15, impact.infectionsByRequestedTime);
+  impact.severeCasesByRequestedTime = percentEstimate(
+    0.15,
+    impact.infectionsByRequestedTime
+  );
+  severeImpact.severeCasesByRequestedTime = percentEstimate(
+    0.15,
+    impact.infectionsByRequestedTime
+  );
 
-  impact.hospitalBedsByRequestedTime = percentEstimate(0.35, totalHospitalBeds) - impact.severeCasesByRequestedTime;
-  severeImpact.hospitalBedsByRequestedTime = percentEstimate(0.35, totalHospitalBeds) - impact.severeCasesByRequestedTime;
+  // Percentage value of Total Hospital Bed
 
-  impact.casesForICUByRequestedTime = percentEstimate(0.05, impact.infectionsByRequestedTime);
-  severeImpact.casesForICUByRequestedTime = percentEstimate(0.05, impact.infectionsByRequestedTime);
-  impact.casesForVentilatorsByRequestedTime = percentEstimate(0.02, impact.infectionsByRequestedTime);
-  severeImpact.casesForVentilatorsByRequestedTime = percentEstimate(0.02, impact.infectionsByRequestedTime);
+  const percentOfValue = percentEstimate(0.35, totalHospitalBeds);
+
+  impact.hospitalBedsByRequestedTime = percentOfValue - impact.severeCasesByRequestedTime;
+
+  severeImpact.hospitalBedsByRequestedTime = percentOfValue - severeImpact.severeCasesByRequestedTime;
+
+  impact.casesForICUByRequestedTime = percentEstimate(
+    0.05,
+    impact.infectionsByRequestedTime
+  );
+  severeImpact.casesForICUByRequestedTime = percentEstimate(
+    0.05,
+    impact.infectionsByRequestedTime
+  );
+  impact.casesForVentilatorsByRequestedTime = percentEstimate(
+    0.02,
+    impact.infectionsByRequestedTime
+  );
+  severeImpact.casesForVentilatorsByRequestedTime = percentEstimate(
+    0.02,
+    impact.infectionsByRequestedTime
+  );
 
   const dollarsInFlight = {};
   const { avgDailyIncomeInUSD, avgDailyIncomePopulation } = region;
 
-  dollarsInFlight.impact = impact.infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * normaliseTimeElapse;
-  dollarsInFlight.severeImpact = severeImpact.infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * normaliseTimeElapse;
+  dollarsInFlight.impact = impact.infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * normaliseTime;
 
+  dollarsInFlight.severeImpact = severeImpact.infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * normaliseTime;
 
-  return { data, impact, severeImpact, dollarsInFlight };
+  console.log('welcome');
+
+   return { data, impact, severeImpact, dollarsInFlight };
 };
 
 export default covid19ImpactEstimator;
